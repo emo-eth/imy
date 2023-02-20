d2cd324062b7a21d60e6cc6d1980e76b761c2ed9
James Wenzel <jameswenzel@users.noreply.github.com>
2023-02-20 14:19:38 -0800

    initial commit

---
+++ src/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+xcode-select --install
+
+# open relevant download links
+open 'https://code.visualstudio.com/download'
+open 'https://www.docker.com/products/docker-desktop/'
+open 'https://openzfsonosx.org/wiki/Downloads'
+open 'https://folivora.ai'
+open 'https://www.macbartender.com'
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# install brew bundle
+brew bundle install
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+rustup
+
+# install foundryup
+curl -L https://foundry.paradigm.xyz | bash
+# install foundry
+foundryup
+
+# install bifrost
+curl -L https://raw.githubusercontent.com/Jon-Becker/heimdall-rs/main/bifrost/install | bash
+# install heimdall
+bifrost
+
+# install huffup
+curl -L get.huff.sh | bash
+huffup
+
+
+echo 'run "(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile" to finish setup'
+
